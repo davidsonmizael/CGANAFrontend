ac52c8534bd02ffbfc18d6ef8b3f812f4166c746
Davidson Mizael <davidsonmizael@gmail.com>
2020-06-08 01:35:02 -0300

    Last update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ImageProcComponent } from './image-proc/image-proc.component';
-import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { SubmitFormComponent } from './submit-form/submit-form.component';
 
 const routes: Routes = [
-  { path: '', component:  HomeComponent},
+  { path: '', component:  ImageProcComponent},
   { path: 'draw', component: ImageProcComponent},
   { path: 'admin', component: AdminComponent},
   { path: 'submit-form', component: SubmitFormComponent}
@@ -17,3 +16,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+  
